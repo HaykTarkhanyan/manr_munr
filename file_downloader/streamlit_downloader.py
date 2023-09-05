7f9a8b0028c6a30f31254e9168fc3702a2790f11
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2023-09-05 18:49:06 +0400

    downloader: makes downloading in the deployed app more convenient

--- file_downloader/streamlit_downloader.py
+++ file_downloader/streamlit_downloader.py
@@ -7,22 +7,22 @@ download_folder = st.text_input('Ո՞ր պապկայի մեջ սեյվ անեմ'
 if not os.path.exists(download_folder):
     os.makedirs(download_folder)
 
-def download(url, index, folder=download_folder):
+def download(url, index, folder=download_folder, output=False):
     file_name = f"{index}. {url.split('/')[-1]}".replace('%20', ' ')
     save_path = os.path.join(folder, file_name)
     
-    st.write(f'Downloading {url} to {save_path}')	
-    
+    if output:
+        st.write(f'Downloading {url} to {save_path}')	
+        
     resp = requests.get(url)
 
     output = open(save_path, 'wb')
     output.write(resp.content)
     output.close()
-    st.download_button(label='Download', data=save_path, file_name=file_name)
 
-    st.write('Download complete.') 
-    
-    
+    if output:
+        st.write('Download complete to server.') 
+        
 
 URLS = {'https://www.cba.am/IMRM/YC/YC.xlsx' : 1, 
         'https://www.cba.am/stat/stat_data_arm/secondary%20market.xlsx': 3, 
@@ -31,11 +31,14 @@ URLS = {'https://www.cba.am/IMRM/YC/YC.xlsx' : 1,
 
 st.title("File Downloader")
 
-if st.button(f"Download files"):
+if st.button('Download All'):
     for u, i in URLS.items():
         download(u, i)
-        
-        
+
+for f in os.listdir(download_folder):
+    st.write(f)
+    st.download_button(label='Download', data=f, file_name=f)
+
 st.markdown("go [here](https://amx.am/en/market_data/government_bonds) and press Download All, name it `2. Bond ...`")
 st.markdown("go [here](https://www.cba.am/AM/SitePages/Default.aspx) and make export webpage to pdf, name it `4 and 7 ...`")
 st.markdown("go [here](https://moneymarket.am/index.php?language=Hay&page=menuinfo&id1=1&id2=1&id3=1) and make export webpage to pdf, name it `6 money market ...`")

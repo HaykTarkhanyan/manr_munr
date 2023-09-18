a1c727da17863c1351b2cdf63a7a0c54c0409ef3
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2023-09-18 22:14:09 +0400

    adds code for stacking qr into a4

--- qr_code/qr_maker.py
+++ qr_code/qr_maker.py
@@ -2,4 +2,24 @@ import segno
 
 link = 'https://linkr.bio/sss_mathematics'
 qrcode = segno.make_qr(link)
-qrcode.save('discord_ugy.png', scale=10, light='#000000', dark='#ffffff')
+qrcode.save('ugy_links_white.png', scale=20)#, light='#ffffff', dark='#ffffff')
+
+
+# stack image in a grid in a4 paper size
+from PIL import Image
+from PIL import ImageFont
+
+# create a new blank image
+new_im = Image.new('RGB', (2480, 3508), color = (255, 255, 255))
+# new_im = Image.new('RGB', (2480, 3508), color = (0, 0, 0))
+
+# get an image
+im1 = Image.open('ugy_links_white.png')
+size = 750
+# paste image at location (0, 0)
+for i in range(0, 5):
+    for j in range(0, 3):
+        new_im.paste(im1, (size*j, i*size))
+
+# save the image
+new_im.save('ugy_links_white_a4.png')
